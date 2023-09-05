84451f785a5bc191ebe7df731700857941e18c72
Gabriel <gc.novaes@outlook.com>
2023-09-05 18:48:55 -0300

    add ex 6

--- ex_6.hs
+++ ex_6.hs
@@ -2,3 +2,17 @@
 -- Exemplo de entrada do "del_rep".
 -- del_rep [ 3 , 1 , 2 , 1 , 2 , 3 ]
 -- >[3 ,1 ,2]
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+
+del_rep :: (Eq x) => [x] -> [x]
+del_rep x = delete x 1
+
+delete :: (Eq x) => [x] -> Int -> [x]
+delete [] _ = []
+delete xs i | i == length xs = xs
+delete (x:xs) i = x : delete ( deleteChar xs x ) (i+1)
+
+deleteChar :: (Eq x) => [x] -> x -> [x]
+deleteChar [] _ = []
+deleteChar xs char = [ x | x <- xs, x /= char ]
