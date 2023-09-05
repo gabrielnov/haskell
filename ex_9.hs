d17c568e5f864c28c6f5b6633f5398b9a5bfbb0f
Gabriel <gc.novaes@outlook.com>
2023-09-05 18:30:30 -0300

    add exercicio remocao

--- ex_9.hs
+++ ex_9.hs
@@ -3,3 +3,13 @@
 -- Exemplo de entrada do "delk".
 -- delk 3 "anonimamentes"
 -- >"anniamnts"
+
+delk :: (Eq x, Ord x) => Int -> [x] -> [x]
+delk _ [] = []
+delk k a = delete k a []
+
+delete :: (Eq x) => Int -> [x] -> [x] -> [x]
+delete k a _ | length a < k = a
+delete k a _ | length a == k = init a
+delete k a b = b ++ delete k (take k a) b ++ delete k (drop k a) b
+
